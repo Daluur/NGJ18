073ecad4edec7462337fc3f875c26633c41e6093
Daluur <fknudsen@gmail.com>
2018-04-14 01:35:02 +0200

    Player movement

---
+++ Assets/Scripts/Players/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	public float Speed = 8;
+	public int Damage = 10;
+	private Vector3 direction;
+
+	public void Setup(Vector3 dir)
+	{
+		direction = dir * Speed;
+	}
+
+	private void Update()
+	{
+		transform.Translate(direction * Time.deltaTime);
+	}
+
+	public void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Enemy")
+		{
+			other.GetComponent<IEnemy>().TakeDamage(Damage);
+			Destroy(gameObject);
+		}
+	}
+}
