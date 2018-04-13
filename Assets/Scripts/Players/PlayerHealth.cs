371e4cf50c8329ab3ba160855d1944ea399ffa64
Daluur <fknudsen@gmail.com>
2018-04-13 22:57:24 +0200

    Basic player script

---
+++ Assets/Scripts/Players/PlayerHealth.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : IPlayer {
+
+	public void TakeDamage(int amount)
+	{
+		Debug.Log("player recieved " + amount + " 'Damage'");
+	}
+}
