419926c4431712f79733ccb43d6d2091fbf4b1de
hubchris <cnelson.private@gmail.com>
2018-04-14 23:22:27 +0200

    extra scenes

---
+++ Assets/movingWall.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movingWall : MonoBehaviour {
+
+	public Vector3 pos1 = new Vector3(0,0,1.25f);
+	public Vector3 pos2 = new Vector3(0,0,-3.8f);
+	public float speed = 0.1f;
+
+	void Update() {
+		transform.position = Vector3.Lerp (pos1, pos2, Mathf.PingPong(Time.time*speed, 1.0f));
+	}
+}
