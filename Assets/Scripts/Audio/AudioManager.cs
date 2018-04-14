9eef30736aa002218d00cf87b3f06374b9b0b12d
Malte Hølledig <malte@holledig.dk>
2018-04-14 11:31:19 +0200

    Added audio manager and folders

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+	private AudioSource audioSource;
+	public AudioClip backgroundMusic;
+
+	void Start () {
+		audioSource = gameObject.GetComponent<AudioSource> ();
+		audioSource.loop = true;
+		audioSource.clip = backgroundMusic;
+		audioSource.Play ();
+	}
+
+	void Update () {
+	}
+}
